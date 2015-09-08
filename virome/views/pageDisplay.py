e49cc182859f60472538889394ee386226d29650
kentnf <kentnf@gmail.com>
2015-09-08 12:38:21 -0400

    fix flist display bug

--- virome/views/pageDisplay.py
+++ virome/views/pageDisplay.py
@@ -43,7 +43,10 @@ def flist(request):
 			context['alt'] = data[fid]['attr'][5]
 			context['fsize'] = data[fid]['attr'][6]
 			context['img'] = data[fid]['attr'][7]
-			context['sample'] = data[fid]['samp']
+			context['sample'] = [];
+			for s in data[fid]['samp']:
+				if s[7] == 1:
+					context['sample'].append(s);
 		else:
 			context['ERRMSG'] = 'field ID ' + fid + ' is not correct'
 	else:
